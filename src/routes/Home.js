cec8b12fb72f0337b567cba079922d5f9517cf87
Nicolas D <ndadevweb@gmail.com>
2021-06-02 23:57:11 +0200

    Mise en place du composant Home

---
+++ src/routes/Home.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+
+const movies = [
+	{
+		backdrop_path: "../../public/images/Fast_large.jpg",
+		id: 475557,
+		overview: "Lorem ipsum...",
+		poster_path: "../../public/images/Fast_small.jpg",
+		title: "Fast and Furious"
+	},
+	{
+		backdrop_path: "../../public/images/Fast_large.jpg",
+		id: 475558,
+		overview: "Lorem ipsum...",
+		poster_path: "../../public/images/Fast_small.jpg",
+		title: "Fast and Furious"
+	},
+	{
+		backdrop_path: "../../public/images/Fast_large.jpg",
+		id: 475559,
+		overview: "Lorem ipsum...",
+		poster_path: "../../public/images/Fast_small.jpg",
+		title: "Fast and Furious"
+	},
+	{
+		backdrop_path: "../../public/images/Fast_large.jpg",
+		id: 475560,
+		overview: "Lorem ipsum...",
+		poster_path: "../../public/images/Fast_small.jpg",
+		title: "Fast and Furious"
+	}
+];
+
+class Home extends Component {
+	render() {
+		return (
+			<div> Home </div>
+		)
+	}
+}
+
+export { Home }
